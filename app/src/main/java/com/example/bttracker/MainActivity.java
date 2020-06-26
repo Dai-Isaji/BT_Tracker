d3a04fc75a354aeb3b03d1b4b845a6d94f6b063e
Dai-Isaji <2020m004@kuas.ac.jp>
2020-06-26 10:40:00 +0900

    Added Mechanism and Normal Activity

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogActivity(View view) {
+      Intent toLog = new Intent(this,LogActivity.class);
+      startActivity(toLog);
+    }
+
+    public void goToNormalActivity(View view) {
+        Intent i1=new Intent(this,NormalActivity.class);
+        startActivity(i1);
+    }
+
+    public void goToMechanism(View view) {
+        Intent i2 = new Intent(this,MechanismActivity.class);
+        startActivity(i2);
     }
 }
