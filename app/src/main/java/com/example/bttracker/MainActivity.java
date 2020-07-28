a98f5c292f4cf930a8716a21a4368fe9eb297552
Dai-Isaji <2020m004@kuas.ac.jp>
2020-07-28 22:40:37 +0900

    finished HW4.1 but lost Mechanism fever

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -6,29 +6,65 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainButton1=findViewById(R.id.MainButton1);
+        mainButton1.setOnClickListener(this);
+
+        TextView mainButton2=findViewById(R.id.MainButton2);
+        mainButton2.setOnClickListener(this);
+
+        TextView mainButton3=findViewById(R.id.MainButton3);
+        mainButton3.setOnClickListener(this);
+
+        TextView mainButton4=findViewById(R.id.MainButton4);
+        mainButton4.setOnClickListener(this);
     }
 
-    public void goToLog(View view) {
-      Intent toLog = new Intent(this,LogActivity.class);
-      startActivity(toLog);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.MainButton1:
+                //If MainButton1 is clicked, do something
+                Intent toLog=new Intent(this, LogActivity.class);
+                startActivity(toLog);
+                break;
+            case R.id.MainButton2:
+                //If MianButton is clicked, do something
+                Intent toNormal=new Intent(this, NormalActivity.class);
+                startActivity(toNormal);
+                break;
+            case R.id.MainButton3:
+                //If MainButton is clicked, do something
+                Intent toMechanism=new Intent(this, MechanismActivity.class);
+                startActivity(toMechanism);
+                break;
+            case R.id.MainButton4:
+                //If MainButton is clicked, do something
+                Intent openFeverLink=new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+                //Verify that the intent openFeverLink will res/olve properly
+                if(openFeverLink.resolveActivity(getPackageManager()) !=null){
+                    startActivity(openFeverLink);
+            }
+                break;
+        }
+
     }
 
-    public void goToNormal(View view) {
-        Intent i1=new Intent(this,NormalActivity.class);
-        startActivity(i1);
-    }
 
-    public void goToMechanism(View view) {
-        Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scientificamerican.com/article/what-causes-a-fever/"));
-        startActivity(i2);
-    }
+
+
+
 
     public void openHandleFeverLink(View view) {
         Intent i3= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
